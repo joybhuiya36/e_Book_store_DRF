0841e1cbe7a35e24fd5f024f09412ebe4802a69e
Joy Bhuiya <joybhuiya36@gmail.com>
2024-10-14 13:50:09 +0600

    chore: Create e_book_store app

--- e_book_store/apps.py
+++ e_book_store/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoCrudOpConfig(AppConfig):
+class EBookStoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Django_CRUD_OP'
+    name = 'e_book_store'
